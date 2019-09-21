25450b227c74ce766a13735b1c434d993bd8c608
zhangyq <zhangyq@citydo.com.cn>
2019-09-21 19:13:13 +0800

    提交

--- src/main/java/com/storage/controller/StockController.java
+++ src/main/java/com/storage/controller/StockController.java
@@ -15,6 +15,9 @@ import java.util.List;
 @RestController
 @RequestMapping("/stock")
 public class StockController {
+    /**
+     * 查询
+     */
     @Resource
     StockService stockService;
     @PostMapping("/select")
